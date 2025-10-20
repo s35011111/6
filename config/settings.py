138385bba4b2153636d836b8888282ca75ff0fdc
Boldina Elena <s35011111@gmail.com>
2025-10-20 14:12:24 +0300

    files for HW_30_1

--- config/settings.py
+++ config/settings.py
@@ -136,9 +136,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL='users.CustomUser'
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
-LOGIN_URL = 'login'
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=BASE_DIR/ 'media'
