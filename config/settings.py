e95f5fa1c65ca69e0664bb46d7d6ee07187eb5b4
Boldina Elena <s35011111@gmail.com>
2026-02-19 13:38:41 +0300

    test 16

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-u9mqi#fi$fc6*if46lkszdsg6)42i!p6(g7n%*t&r-$hggag9q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
